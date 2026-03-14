897398f921074e4e33c5eecce6c64fd62cbd1ee0
hgy4152 <hgy4152@gmail.com>
2026-03-14 17:00:47 +0900

    CondingPractice47
    제네릭 예제(queue)

--- GenericStack/Program.cs
+++ GenericStack/Program.cs
@@ -1,4 +1,39 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+
+SimpleQueue<int> intQueue = new SimpleQueue<int>(3);
+SimpleQueue<string> stringQueue = new SimpleQueue<string>(2);
+
+Console.WriteLine("=== int 큐 (용량: 3) ===");
+
+intQueue.Enqueue(10);
+intQueue.Enqueue(20);
+intQueue.Enqueue(30);
+
+Console.WriteLine($"Enqueue:{string.Join(", ", intQueue.Queue)}");
+
+Console.WriteLine($"Count: {intQueue.Count}, IsFull: {intQueue.IsFull}");
+intQueue.Enqueue(50);
+
+Console.WriteLine($"Peek: {intQueue.Peek()}");
+Console.WriteLine($"Dequeue: {intQueue.Dequeue()}");
+Console.WriteLine($"Dequeue: {intQueue.Dequeue()}");
+Console.WriteLine($"Count: {intQueue.Count}, IsEmpty: {intQueue.IsEmpty}");
+
+
+Console.WriteLine();
+
+Console.WriteLine("=== string 큐 (용량: 2) ===");
+
+stringQueue.Enqueue("Hello");
+stringQueue.Enqueue("World");
+
+Console.WriteLine($"Enqueue:{string.Join(", ", stringQueue.Queue)}");
+
+
+Console.WriteLine($"Dequeue: {stringQueue.Dequeue()}");
+Console.WriteLine($"Dequeue: {stringQueue.Dequeue()}");
+Console.WriteLine($"Dequeue: {stringQueue.Dequeue()}");
+Console.WriteLine($"isEmpty: {stringQueue.IsEmpty}");
---
+++ GenericStack/SimpleQueue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+class SimpleQueue<T>
+{
+    private int Capacity;
+
+    public int Count { get { return Queue.Count; } }
+    public bool IsFull {  get { return Queue.Count == Capacity; } }
+    public bool IsEmpty { get {  return Queue.Count == 0; }  }
+    public List<T> Queue;
+
+    public SimpleQueue(int capacity)
+    {
+        Capacity = capacity;
+        Queue = new List<T>();
+    }
+
+    public T Enqueue(T item)
+    {
+        if(IsFull)
+        {
+            Console.WriteLine("큐가 가득 찼습니다.");
+            return default(T);
+        }
+
+        Queue.Add(item);
+
+        return item;
+    }
+
+
+    public T Dequeue()
+    {
+
+        if (IsEmpty)
+        {
+            Console.WriteLine("큐가 비어있습니다.");
+            return default(T);
+        }
+
+
+        T item = Queue[0];
+        
+        for (int i = 1; i < Count; i++)
+        {
+            Queue[i-1] = Queue[i];
+        }
+        Queue.RemoveAt(Count-1);
+
+
+        return item;
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty)
+        {
+            Console.WriteLine("큐가 비어있습니다.");
+            return default(T);
+        }
+
+        return Queue[0];
+
+    }
+
+
+}
